eb8bb620dd6d8c9e7a758a2023356bfacd910612
RichardRoda <RichardRoda@users.noreply.github.com>
2017-05-06 23:13:32 -0400

    Complete discussion of Functional Interfaces (FIs).  Starting discussion of streams.

--- sample/examples/src/main/java/com/mycompany/mavenproject1/Example1.java
+++ sample/examples/src/main/java/com/mycompany/mavenproject1/Example1.java
@@ -13,7 +13,21 @@ import java.util.function.Predicate;
  */
 public class Example1 {
     static public void main(String[] args) throws Exception {
-        Predicate<Integer> isFive = n -> n == 5;
-        System.out.println(isFive.test(4)); // false
+        
+
+    }
+    
+    static void exampleA() {
+Predicate<Integer> isFive = n -> n == 5;
+System.out.println(isFive.test(4)); // false        
+    }
+    
+    static void exampleB() {
+        Predicate<Integer> isFour = mkTestFunc(4);
+        System.out.println(isFour.test(4)); // true        
+    }
+    
+    public static Predicate<Integer> mkTestFunc(int value) {
+        return n -> n == value;
     }
 }
