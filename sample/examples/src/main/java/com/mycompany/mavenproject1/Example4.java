16dc10dc2cdffb3caf2e002eb520dd85deeeea95
RichardRoda <RichardRoda@users.noreply.github.com>
2017-05-06 23:55:53 -0400

    Add Example4.java.  Clean up definition of stream.  Example will be on next slide showing the lambda in the stream.

---
+++ sample/examples/src/main/java/com/mycompany/mavenproject1/Example4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.util.stream.IntStream;
+
+/**
+ *
+ * @author Richard
+ */
+public class Example4 {
+    static public void main(String[] args) {
+        System.out.print(IntStream.iterate(0, i -> i+1)
+                .limit(12).sum());
+    }
+}
