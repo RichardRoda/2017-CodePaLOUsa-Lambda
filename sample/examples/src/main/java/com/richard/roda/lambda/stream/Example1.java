5644b3ecc1d0c895adf77673ac79874e1b9ba894
RichardRoda <RichardRoda@users.noreply.github.com>
2017-05-16 23:54:45 -0400

    Completed slides, first cut.

--- sample/examples/src/main/java/com/richard/roda/lambda/stream/Example1.java
+++ sample/examples/src/main/java/com/richard/roda/lambda/stream/Example1.java
@@ -5,7 +5,12 @@
  */
 package com.richard.roda.lambda.stream;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 /**
  *
@@ -16,6 +21,11 @@ public class Example1 {
     static public void main(String[] args) throws Exception {
         exampleA();
         exampleB();
+        staticMethodReference();
+        specificInstanceMethodReference();
+        System.out.println();
+        constructorMethodReference();
+        instanceMethodReference();
     }
 
     static void exampleA() {
@@ -28,6 +38,30 @@ public class Example1 {
         System.out.println(isFour.test(4)); // true        
     }
 
+    static void staticMethodReference() {
+        Function<char[],String> valueOf = String::valueOf;
+        String value = valueOf.apply(new char[] {'H','e','l','l','o'});
+        System.out.println(value); // Hello
+    }
+    
+    static void specificInstanceMethodReference() {
+        Consumer<Object> printer = System.out::print;
+        Arrays.asList(1,2,3,4).forEach(printer);
+    }
+    
+    static void constructorMethodReference() {
+        Supplier<StringBuilder> supplier = StringBuilder::new;
+        // supplier = () -> new StringBuilder();
+        StringBuilder sb = supplier.get().append("Hi!");
+        System.out.println(sb); // Hi!
+    }
+    
+    static void instanceMethodReference() {
+        UnaryOperator<String> toUpperCase = String::toUpperCase;
+        // toUpperCase -> s -> s.toUpperCase();
+        System.out.println(toUpperCase.apply("abc"));
+    }
+    
     public static Predicate<Integer> mkTestFunc(int value) {
         return n -> n == value;
     }
